15419a0c41b52ebe7f469cd0b5ba3937cd0e3d98
benoldenburg <oldenburg2@gmail.com>
2023-10-25 07:04:12 -0400

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,19 +3,34 @@ import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import {
+  PageHeader,
+  Section,
+  Content,
+  Row,
+  Col,
+} from "../styles/StyledElements"
+import { PageSimpleHeader } from "../components/pageSimpleHeader"
 
 const NotFoundPage = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
 
   return (
     <Layout location={location} title={siteTitle}>
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <Seo title={"404: Not Found"} />
+      <PageSimpleHeader title={"404: Not Found"}></PageSimpleHeader>
+      <Section>
+        <Content>
+          <Row>
+            <Col>
+              <p>You just landed on a route that doesn&#39;t exist.</p>
+            </Col>
+          </Row>
+        </Content>
+      </Section>
     </Layout>
   )
 }
-
-export const Head = () => <Seo title="404: Not Found" />
 
 export default NotFoundPage
 

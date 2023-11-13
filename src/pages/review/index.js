3e534ab285a1af982699c62e9a88ee5b5aada71c
benoldenburg <oldenburg2@gmail.com>
2023-11-12 21:31:55 -0500

    Update index.js

--- src/pages/review/index.js
+++ src/pages/review/index.js
@@ -42,10 +42,6 @@ const SiteIndex = ({ data, location }) => {
       <Section>
         <Content>
           <h1>Review Index</h1>
-          <p>
-            This is a utilty to review all current atlas entries. In other page
-            listings only posts with Status value set to "Complete" will appear
-          </p>
         </Content>
       </Section>
       <Section>
